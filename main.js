6f34d2dec04e51481219f8af8f75e380dab876eb
Edmar <edmar.contato149@gmail.com>
2024-01-25 20:14:02 -0300

    fixed

--- main.js
+++ main.js
@@ -20,4 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
             reposElement.innerText = json.public_repos; 
             linkElement.href = json.html_url; 
         })
+        .catch(function(erro) {
+            alert("Ocorreu um erro ao buscar o endereço, tente novamente mais tarde.")
+        })
 })
